231c736f981269dfba2d1ea94a0e31428e0ee7ba
Jan Piotrowski <piotrowski+github@gmail.com>
2022-10-07 22:51:15 +0000

    again

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,23 +1,11 @@
 import * as trpc from "@trpc/server";
 import { z } from "zod";
-//import { prisma } from "@/backend/utils/prisma";
-import { getOptionsForVote } from "@/utils/getRandomPokemon";
 
 export const appRouter = trpc
   .router()
   .query("get-pokemon-pair", {
     async resolve() {
-      return { foo: "bar" }
-    },
-  })
-  .mutation("cast-vote", {
-    input: z.object({
-      votedFor: z.number(),
-      votedAgainst: z.number(),
-    }),
-    async resolve({ input }) {
-      const voteInDb = undefined
-      return { success: true, vote: voteInDb };
+      return {"firstPokemon":{"id":2,"name":"ivysaur","spriteUrl":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png"},"secondPokemon":{"id":129,"name":"magikarp","spriteUrl":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/129.png"}};
     },
   });
 
